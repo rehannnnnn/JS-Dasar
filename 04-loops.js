0e147c9c4027fae947b8f79409437f2dd0229c84
rehannnnnnn <61625427+rehannnnnn@users.noreply.github.com>
2021-09-29 20:01:09 +0700

    Create 04-loops.js

---
+++ 04-loops.js
@@ -0,0 +1,33 @@
+/// Soal - 01
+/// Kamu diminta untuk membuat sebuah perulangan menggunakan for loop, untuk mencetak angka prima dari 1 ~ 100
+/// EDIT HERE
+function isPrime(num) {
+    if(num < 2) return false;
+    for (var i = 2; i < num; i++) {
+        if(num%i==0)
+            return false;
+    }
+    return true;
+}
+for(var i = 0; i < 100; i++){
+    if(isPrime(i)) console.log(i);
+}
+
+/// Soal - 02
+/// Kamu diminta untuk menemukan bilangan prima ke-50, temukan bilangan tersebut menggunakan while loop 
+let primeCounter = 0;
+let fiftiethPrime;
+
+/// EDIT HERE
+while () {
+    
+}
+
+
+/// Soal - 03
+/// Kamu diminta untuk menemukan bilangan ganjil ke-50, temukan bilangan tersebut menggunakan do while loop
+let oddCounter = 0;
+let fiftiethOdd;
+
+/// EDIT HERE
+do { .... } while (....)
